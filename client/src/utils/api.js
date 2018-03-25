d84c22e37cee0ce2cd1b64a7af231a09bef0e280
Josh Silverman <jrsilverman71@hotmail.com>
2018-03-24 18:44:13 -0700

    added api/axios

---
+++ client/src/utils/api.js
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const api = {
+  // Query NYT API
+  searchNYT: function(topic, startYear, endYear) {
+    const authKey = "b4496cb9f412477a900745d0d4dfd3c3";
+    const queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" +
+    authKey + "&q=" + topic + "&begin_date=" + startYear + "0101&end_date=" + endYear + "0101";
+    return axios.get(queryURL);
+  },
+  // Retrieves saved articles from the db
+  getArticle: function() {
+    return axios.get("/api/saved");
+  },
+  // Saves a new article to the db
+  saveArticle: function(articleObj) {
+    return axios.post("/api/saved", articleObj);
+  },
+  // Deletes an article from the db
+  deleteArticle: function(id) {
+    return axios.delete(`/api/saved/${id}`);
+  }
+};
+
+export default api;
