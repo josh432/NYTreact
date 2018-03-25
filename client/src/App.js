237109bd0712a97916e340cf41087b1a1f8a4d47
Josh Silverman <jrsilverman71@hotmail.com>
2018-03-25 11:26:30 -0700

    started building components, connected app.js to components using react-router, added router json

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,13 @@
-import React, { Component } from 'react';
-
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
+import Main from "./components/Main";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () =>
+  <Router>
+    <div>
+      <Route path="/" component={Main} />
+    </div>
+  </Router>;
 
 export default App;
